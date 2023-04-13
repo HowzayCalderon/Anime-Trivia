baa9ab5d04463aa79db3b53aeefd2b1fc537c9fd
hairyElephant <josecalderon353@gmail.com>
2023-04-13 19:46:29 -0400

    needs winning and losing clause and bug fix

--- apps.js
+++ apps.js
@@ -20,26 +20,31 @@ let questions = [
         options: ['Elric Brothers', 'Paradox Brothers', 'Uchiha Brothers', 'Zoldyck Brothers'],
         answer: 'Elric Brothers'
     },
-    // {
-    //     que: "What is the name of the talking cat Goku visits?",
-    //     answer: "korin"
-    // },
-    // {
-    //     que: "In which dragonball series was super saiyan 4 first seen?",
-    //     answer: "dragonball gt"
-    // },
-    // {
-    //     que: "Who was the One-Eyed Owl in Tokyo Ghoul?",
-    //     answer: "eto"
-    // },
-    // {
-    //     que: "Does Asta from Black Clover have any magic ability?",
-    //     answer: 'no'
-    // },
-    // {
-    //     que: "Which two people were in possesion of the Colossal Titan?",
-    //     answer: "armin, bertholdt"
-    // }
+    {
+        que: "What is the name of the talking cat Goku visits in DragonBall?",
+        options: ['Mishu', 'Garfield', 'Beerus', 'Korin'],
+        answer: "Korin"
+    },
+    {
+        que: "In which dragonball series was super saiyan 4 first seen?",
+        answer: 'DragonBall GT',
+        options: ['DragonBall', 'DragonBall Z', 'DragonBall GT', 'DragonBall Super']
+    },
+    {
+        que: "Who was the One-Eyed Owl in Tokyo Ghoul?",
+        answer: 'Eto',
+        options: ['Ken Kaneki', 'Eto', 'Yoshimura', 'Touka' ]
+    },
+    {
+        que: "What is Asta from Black Clover's magic ability?",
+        answer: 'Anti-Magic',
+        options: ['Barrier Magic', 'Creation Magic', 'Healing Magic', 'Anti-Magic']
+    },
+    {
+        que: "Who was the first person in possesion of the Colossal Titan?",
+        answer: 'Bertholdt',
+        options: ['Bertholdt', 'Armin', 'Eren Yeager', 'Mikasa']
+    }
 ]
 
 const btn1 = document.querySelector('#btn1');
@@ -53,71 +58,94 @@ const points = document.querySelector('.points');
 const question = document.querySelector('.mainQuestion')
 const strtBtn = document.querySelector('.startButton')
 
-strtBtn.addEventListener('click', theGame())
+window.addEventListener('load', theGame())
 
+function setQuestion(){
+    questions.length <= 0 ? console.log('done') :
+    question.textContent = questions[0].que;
+    btn1.textContent = questions[0].options[0]
+    btn2.textContent = questions[0].options[1]
+    btn3.textContent = questions[0].options[2]
+    btn4.textContent = questions[0].options[3]
 
+    btn1.className = 'btn';
+    btn2.className = 'btn';
+    btn3.className = 'btn';
+    btn4.className = 'btn';
+}
+
+    
 function theGame(){
     lives.textContent = 4;
     points.textContent = 0
 
-    function setQuestion(){
-        question.textContent = questions[0].que;
-        btn1.textContent = questions[0].options[0];
-        btn2.textContent = questions[0].options[1];
-        btn3.textContent = questions[0].options[2];
-        btn4.textContent = questions[0].options[3];
+    question.textContent = questions[0].que
+    btn1.textContent = questions[0].options[0]
+    btn2.textContent = questions[0].options[1]
+    btn3.textContent = questions[0].options[2]
+    btn4.textContent = questions[0].options[3]
 
-        btn1.className = 'btn';
-        btn2.className = 'btn';
-        btn3.className = 'btn';
-        btn4.className = 'btn';
-    }
+        
+        btn1.addEventListener('click', () => {
+            if(btn1.textContent == questions[0].answer){
+                questions.length <= 0 && points.textContent >= 8 ? console.log('Done') : 
+                btn1.className = 'btn-correct';
+                questions.shift();
+                setInterval(setQuestion, 1000)
+                points.textContent++;
+            }else{
+                btn1.className = 'btn-wrong';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                lives.textContent--
+            }
+        })
 
-    question.textContent = questions[0].que;
-    btn1.textContent = questions[0].options[0];
-    btn2.textContent = questions[0].options[1];
-    btn3.textContent = questions[0].options[2];
-    btn4.textContent = questions[0].options[3];
-    
-    btn1.addEventListener('click', () => {
-        if(btn1.textContent == questions[0].answer){
-            btn1.className = 'btn-correct';
-            questions.shift();
-            setInterval(setQuestion, 1000);
-            points.textContent++
-        }else{
-            btn1.className = 'btn-wrong';
-            questions.shift();
-            setInterval(setQuestion, 1000);
-            lives.textContent--
-        }
-    })
+        btn2.addEventListener('click', () => {
+            if(btn2.textContent == questions[0].answer){
+                questions.length <= 0 ? console.log('Done') : 
+                btn2.className = 'btn-correct';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                points.textContent ++;
+            }else{
+                btn2.className = 'btn-wrong';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                lives.textContent--
+            }
+        })
 
-    btn2.addEventListener('click', () => {
-        if(btn2.textContent == questions[0].answer){
-            btn2.className = 'btn-correct';
-            questions.shift();
-            setInterval(setQuestion, 1000);
-            points.textContent ++
-        }
-    })
+        btn3.addEventListener('click', () => {
+            if(btn3.textContent == questions[0].answer){
+                questions.length <= 0 ? console.log('Done'):
+                btn3.className = 'btn-correct';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                points.textContent ++;
+            }else{
+                btn3.className = 'btn-wrong';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                lives.textContent--
+            }
+        })
 
-    btn3.addEventListener('click', () => {
-        if(btn3.textContent == questions[0].answer){
-            btn3.className = 'btn-correct';
-            questions.shift();
-            setInterval(setQuestion, 1000);
-            points.textContent ++
-        }
-    })
-
-    btn4.addEventListener('click', () => {
-        if(btn4.textContent == questions[0].answer){
-            btn4.className = 'btn-correct';
-            questions.shift();
-            setInterval(setQuestion, 1000);
-            points.textContent ++
-        }
-    })
+        btn4.addEventListener('click', () => {
+            if(btn4.textContent == questions[0].answer){
+                questions.length <= 0 ? console.log('Done') : 
+                btn4.className = 'btn-correct';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                points.textContent ++;
+            }else{
+                btn4.className = 'btn-wrong';
+                questions.shift();
+                setInterval(setQuestion, 1000);
+                lives.textContent--
+            }
+        })
     
 }
+
+// when you're out of questions you get an infinite loop, figure out why and stop it 
